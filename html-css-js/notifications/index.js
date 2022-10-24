3d708b1b487cc10708a07c9d00591e4ced2eb9a8
Martin Rakesh A Metharam <Martin.Metharam@sykes.com>
2022-10-24 15:55:18 +0800

    Initial commit.

---
+++ html-css-js/notifications/index.js
@@ -0,0 +1,31 @@
+// How To Send Push Notifications With JavaScript
+// https://www.youtube.com/watch?v=Bm0JjR4kP8w&list=WL&index=3
+// https://developer.mozilla.org/en-US/docs/Web/API/notification
+
+const btnSend = document.querySelector('#btnSend');
+
+btnSend.addEventListener('click', async () => {
+    // Check if `Notifications` is supported.
+    if ('Notification' in window) {
+        alert('Notifications is not supported!');
+        return;
+    }
+
+    if (Notification.permission != 'granted') {
+        console.log('Denied!');
+        return;
+    }
+
+    console.log('Granted!');
+
+    new Notification('Hello!');
+
+    // const notification = new Notification('My Title', {
+    //     body: 'A sample body in the notification',
+    //     data: { name: 'Adam Smith', message: 'Hello World!!!' }
+    // });
+
+    // notification.addEventListener('close', (e) => {
+    //     console.log(e);
+    // });
+});
