7615d1301e4fb762898bac40e6e5235b29576634
Martin Rakesh A Metharam <Martin.Metharam@sykes.com>
2022-12-03 20:11:11 +0800

    Modal popup

---
+++ html-css-js/modal-popups/index.js
@@ -0,0 +1,42 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.querySelector('#overlay');
+
+openModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+
+        openModal(modal);
+    });
+});
+
+closeModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // Get the closest parent that has a class of modal.
+        const modal = button.closest('.modal');
+
+        closeModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+
+    modals.forEach((modal) => {
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (!modal) return;
+
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (!modal) return;
+
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
